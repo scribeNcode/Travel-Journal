5c369a71e26f4473af0adfaf7d0c91470df010db
scribencode <deyeye99@gmail.com>
2026-04-25 10:10:21 +0100

    feat:implement responsive header

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import Logo from "../assets/Icons/Logo.svg";
+
+function Header() {
+  return (
+    <div className="font-league">
+      <header>
+        <div className="bg-primary flex justify-center items-center h-13.75 gap-2 text-bg">
+          <img src={Logo} alt="website logo" />
+          <h1>my travel journal</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
